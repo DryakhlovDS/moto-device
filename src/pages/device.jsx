2c7e1a8246b5a540103d22b2497db04b70df8de3
Дмитрий Дряхлов <dds@digtlab.ru>
2021-08-07 11:05:27 +0300

    add slider

--- src/pages/device.jsx
+++ src/pages/device.jsx
@@ -5,6 +5,7 @@ import { BasketContext } from "../store/BasketStore";
 import { DevicesContext } from "../store/DevicesStore";
 import { fetchOneDevice } from "../http/deviceAPI";
 import { observer } from "mobx-react-lite";
+import SimpleImageSlider from "react-simple-image-slider";
 
 const Device = observer(() => {
   const { devices } = useContext(DevicesContext);
@@ -31,22 +32,39 @@ const Device = observer(() => {
     addToCart(idDevice);
     history.push("/basket");
   };
-  console.log(device);
+
+  const slides = [{ url: process.env.REACT_APP_API_URL + "/" + device.img }];
+
+  device.photos &&
+    device.photos.forEach((img) =>
+      slides.push({
+        url: `${process.env.REACT_APP_API_URL}/${device.name}/${img}`,
+      })
+    );
+
   return (
     <section className='device'>
       <div className='container'>
         <div className='device__inner'>
           <h2>{device.name}</h2>
-          <img
-            src={process.env.REACT_APP_API_URL + "/" + device.img}
-            alt='device'
-          />
-          {device.photos.map((img) => (
-            <img
-              src={`${process.env.REACT_APP_API_URL}/${device.name}/${img}`}
-              alt={device.name}
+
+          {device.photos && device.photos.length ? (
+            <SimpleImageSlider
+              width={480}
+              height={270}
+              images={slides}
+              showBullets={true}
+              showNavs={true}
+              navSize={30}
+              navMargin={10}
+              style={{ margin: "0 auto" }}
             />
-          ))}
+          ) : (
+            <img
+              src={process.env.REACT_APP_API_URL + "/" + device.img}
+              alt='device'
+            />
+          )}
           <p>{device.descriptionFull}</p>
           <table className='device__props'>
             <thead>
